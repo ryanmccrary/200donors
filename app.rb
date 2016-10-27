3ce0692fe30e527260e37c41ed1191e2eeccaf95
rrmccrary@gmail.com <rrmccrary@gmail.com>
2016-10-27 12:08:41 -0400

    First commit for 2016

--- app.rb
+++ app.rb
@@ -165,7 +165,7 @@ __END__
       <h1>Give the Gift
         <span>of the Outdoors</span>
       </h1>
-      <p class="description">Join us this Christmas to give the gift of the outdoors to someone who can’t get there on their own. Our goal is to have someone claim each of the values below by the end of the year. It’s simple - if every amount below is donated, we can raise $20,000 for our 2016 programs without a single person having to give over $200. So go ahead, pick your amount and give a life changing experience to a kid through GOAT!</p>
+      <p class="description">Join us this Christmas to give the gift of the outdoors to someone who can’t get there on their own. Our goal is to have someone claim each of the values below by the end of the year. It’s simple - if every amount below is donated, we can raise $20,000 for our 2017 programs without a single person having to give over $200. So go ahead, pick your amount and give a life changing experience to a kid through GOAT!</p>
     </div>
   </header>
 
